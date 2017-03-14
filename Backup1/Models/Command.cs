617603ec01a000856333dd9fd8ddc459c8fdecbf
TP <vankampen@evident.nl>
2017-03-14 22:07:01 +0100

    Another commit

---
+++ Backup1/Models/Command.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Hueliday
+{
+	public class Command
+	{
+		public string Address { get; set; } //Path to a light resource, a group resource or any other bridge resource (including "/api/<username>/")
+		public string Method { get; set; } // GET, PUT, ...
+		public object Body { get; set; } //json body of the command
+
+	}
+}
---
+++ Backup1/Models/Helpers/LowecaseContractResolver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json.Serialization;
+
+namespace Hueliday
+{
+	public class LowercaseContractResolver : DefaultContractResolver
+	{
+		protected override string ResolvePropertyName(string propertyName)
+		{
+			return propertyName.ToLower();
+		}
+	}
+}
