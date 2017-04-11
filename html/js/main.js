a697ff7d2a15aa8c2bbde96c7ea0f05de605aee7
Theo-Paul van Kampen <github@van-kampen.net>
2017-04-11 22:13:11 +0200

    this works

--- html/js/main.js
+++ html/js/main.js
@@ -1,7 +1,17 @@
-$(document).ready(function() {
-	$(".col-md-1").css('background-color', '#eee');
-
-
+// the HueBridge object holding all Hue functions
+var HueBridge = (function () {
+	// return all schedules as an array of schedule-objects
+	var getSchedules = (cfg, callback) => {
+		$.getJSON(cfg.url+cfg.schedules, function(allSchedules, textStatus) {
+			var schedules = [];
+			$.each(allSchedules, function(index, val) {
+				schedules.push(val);
+			});
+			if (typeof callback == 'function')
+				callback(schedules);
+		});
+	};
+	
 	// render the html for a single Schedule object
 	var renderHtml = (s) => {
 		return ("<span class=\"schedule\">Name: " + s.name + "</span></br> \
@@ -9,36 +19,35 @@ $(document).ready(function() {
 			<span class=\"schedule\">Command: " + JSON.stringify(s.command) + "</span></br> \
 			<span class=\"schedule\">Localtime: " + s.localtime + "</span></br> \
 			");
-	}
+	};
 
-	// return all schedules as an array of schedule-objects
-	var getSchedules = function(schedules, cfg)
-	{
-		$.getJSON(cfg.url+cfg.schedules, function(allSchedules, textStatus) {
-			$.each(allSchedules, function(index, val) {
-				schedules.push(val);
-			});
-		});
+	var config = {
+		url			: "http://192.168.2.12/api/newdeveloper/",
+		prefix		: "HH_",
+		schedules 	: "schedules"
 	}
+	return {
+		getSchedules	: getSchedules,
+		renderHtml		: renderHtml,
+		config			:  config
+	}
+})();
+
+$(document).ready(function() {
+	$(".col-md-1").css('background-color', '#eee');
 
 	// main function
 	var main = function() {
-		var schedules = [];
-		var htmlLines = [];
-		var config = {
-			url			: "http://192.168.2.12/api/newdeveloper/",
-			prefix		: "HH_",
-			schedules 	: "schedules"
-		};
 
-		getSchedules(schedules, config);
-
-		for (var schedule in schedules)
-		{
-			htmlLines.push("<li>" + renderHtml(schedule) + "</li>");
-		}
-		$("ul").append(htmlLines.join(""));
-
+		// get schedules from Hue, renderHTML each schedule if it's a Holiday schedule
+		HueBridge.getSchedules(HueBridge.config, (sch) => {
+			var htmlLines = [];
+			$.each(sch, function (index, val) {
+				// only holiday schedules
+				if (val.name.startsWith(HueBridge.config.prefix))
+					htmlLines.push("<li>" + HueBridge.renderHtml(val) + "</li>");
+			});
+			$("ul").append(htmlLines.join(""));
+		});
 	}();
-
 });
